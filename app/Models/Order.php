e86ae57a1e6796a0a48a27e160a379dec45789e5
Martin Wheeldon <martin.wheeldon@golfnow.co.uk>
2023-09-06 10:23:32 +0100

    First

---
+++ app/Models/Order.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    public $fillable = ['customer_id', 'product_id'];
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
